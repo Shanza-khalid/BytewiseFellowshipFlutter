421051a62527fddf2e80bfa832b58b6d00135881
Shanza-khalid <128227443+Shanza-khalid@users.noreply.github.com>
2023-03-28 08:24:56 -0700

    .

---
+++ week2/datatype.dart
@@ -0,0 +1,24 @@
+main(){
+
+  int amount1 = 10;
+  var amount2 = 20;
+  print('Amount 1: $amount1 | Amount 2: $amount2 \n');
+
+  double damount1 = 10.10;
+  var damount2 = 20.20;
+  print('dAmount1: $damount1 | dAmount2: $damount2 \n');
+
+  String firstname = 'shanza';
+  var   lastname = 'khalid';
+  print ('My name is: $firstname $lastname \n');
+
+   bool isittrue1 = true;
+   var  isittrue2 = false;
+   print('isittrue1: $isittrue1 | isittrue2: $isittrue2 \n');
+
+   dynamic  weakvariable = 100;
+   print('Weak variable: $weakvariable \n');
+   
+   weakvariable = 'Dart Programming';
+   print('Weak variable 2: $weakvariable');
+}
